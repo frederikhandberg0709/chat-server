604ca0b78505a3356f4f0a7c550a678c635f904d
FREDERIK DIAMOND <hello@frederikhandberg.com>
2025-05-08 20:42:37 +0200

    Setting up Spring Boot project

---
+++ rest-server/src/test/java/com/frederikhandberg/chat_server/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frederikhandberg.chat_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
