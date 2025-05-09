d136902a444c40b19e0dc5cdf280624f754de0ab
FREDERIK DIAMOND <hello@frederikhandberg.com>
2025-05-09 21:59:04 +0200

    User model added

---
+++ rest-server/src/main/java/com/frederikhandberg/model/User.java
@@ -0,0 +1,59 @@
+package com.frederikhandberg.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Index;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users", indexes = {
+        @Index(name = "idx_user_email", columnList = "email"),
+        @Index(name = "idx_user_username", columnList = "username")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 50)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank
+    @Size(min = 3, max = 30)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @JsonIgnore
+    private String password;
+}
