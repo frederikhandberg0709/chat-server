b9b29b200a8cefda487e0098c29f071e81c9ce3b
FREDERIK DIAMOND <hello@frederikhandberg.com>
2025-06-22 00:37:03 +0200

    Added HTTP method to check if user is logged in
    Needs to be tested

--- backend-server/src/main/java/com/frederikhandberg/controller/UserController.java
+++ backend-server/src/main/java/com/frederikhandberg/controller/UserController.java
@@ -1,21 +1,48 @@
 package com.frederikhandberg.controller;
 
+import com.frederikhandberg.adapter.UserDetailsImpl;
+import com.frederikhandberg.model.User;
 import java.time.LocalDateTime;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.frederikhandberg.model.User;
-
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getCurrentUser(
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        User currentUser = userDetails.getUser();
+
+        if (currentUser == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return ResponseEntity.ok(currentUser);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id) {
-        return ResponseEntity.ok(new User(id, "John", "Doe", "johndoe", "test@mail.com", "password",
-                LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now()));
+        return ResponseEntity.ok(
+            new User(
+                id,
+                "John",
+                "Doe",
+                "johndoe",
+                "test@mail.com",
+                "password",
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                LocalDateTime.now()
+            )
+        );
     }
 }
